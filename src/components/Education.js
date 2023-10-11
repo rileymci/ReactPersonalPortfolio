a62270f8cdb9215fe4af376e50018e6ab1263801
rileymci <riley.mcilwain@connect.qut.edu.au>
2023-10-11 12:18:39 +1000

    Mobile changes

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,4 +1,6 @@
 import { Col, Container, Row } from "react-bootstrap"
+import Carousel from 'react-bootstrap/Carousel';
+import ExampleCarouselImage from "../assets/img/C.png"
 
 
 export const Education = () => {
@@ -6,11 +8,7 @@ export const Education = () => {
 
     return(
         <section className="education" id="education">
-            <Row>
-                <Col>
                 <h2>Education</h2>
-                </Col>
-            </Row>
         </section>
     )
 
