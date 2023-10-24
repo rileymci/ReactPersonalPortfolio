afa6235ca7c2bc2adac832e604165d7a11bfb9c6
rileymci <riley.mcilwain@connect.qut.edu.au>
2023-10-24 15:04:59 +1000

    Education Update

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,6 +1,9 @@
 import { Col, Container, Row } from "react-bootstrap"
 import Carousel from 'react-bootstrap/Carousel';
-import ExampleCarouselImage from "../assets/img/C.png"
+import "react-multi-carousel/lib/styles.css";
+import qut from "../assets/img/bannerq.png";
+import rochedale from "../assets/img/rochey.jpg";
+
 
 
 export const Education = () => {
@@ -8,8 +11,31 @@ export const Education = () => {
 
     return(
         <section className="education" id="education">
-                <h2>Education</h2>
+        <Container>
+        <h2>Education</h2>
+        <Carousel>
+                <Carousel.Item interval={3000}>
+                    <img style={{height:'50vh'}}
+                    className="d-block w-100" 
+                    src={qut}
+                    />
+                    <Carousel.Caption>
+                        <h3>Queensland University of Technology</h3>
+                        <p></p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                    <img style={{height:'50vh'}}
+                    className="d-block w-100" 
+                    src={rochedale}
+                    />
+                    <Carousel.Caption>
+                        <h3>Rochedale State Highschool</h3>
+                        <p></p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+            </Container>
         </section>
     )
-
 }
