7cf7f0baede8b6cb304f46fc4df355b28132c9e3
rileymci <riley.mcilwain@connect.qut.edu.au>
2023-10-24 18:30:22 +1000

    Education

--- src/components/Education.js
+++ src/components/Education.js
@@ -3,6 +3,8 @@ import Carousel from 'react-bootstrap/Carousel';
 import "react-multi-carousel/lib/styles.css";
 import qut from "../assets/img/bannerq.png";
 import rochedale from "../assets/img/rochey.jpg";
+import rochedale2 from "../assets/img/rochedaleban.jpg";
+import colorSharp from "../assets/img/color-sharp.png"
 
 
 
@@ -12,14 +14,15 @@ export const Education = () => {
     return(
         <section className="education" id="education">
         <Container>
+        <div className="education-bx">
         <h2>Education</h2>
-        <Carousel>
+        <Carousel className="education-slider">
                 <Carousel.Item interval={3000}>
                     <img style={{height:'50vh'}}
                     className="d-block w-100" 
                     src={qut}
                     />
-                    <Carousel.Caption>
+                    <Carousel.Caption className="education-caption">
                         <h3>Queensland University of Technology</h3>
                         <p></p>
                     </Carousel.Caption>
@@ -27,15 +30,17 @@ export const Education = () => {
                 <Carousel.Item interval={3000}>
                     <img style={{height:'50vh'}}
                     className="d-block w-100" 
-                    src={rochedale}
+                    src={rochedale2}
                     />
-                    <Carousel.Caption>
-                        <h3>Rochedale State Highschool</h3>
+                    <Carousel.Caption className="education-caption">
+                        <h3>Rochedale State High School</h3>
                         <p></p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
+            </div>
             </Container>
+            <img className="background-image-left2" src={colorSharp}></img>
         </section>
     )
 }
