dea9e45f6aed1c669813fe85b648381b9850eadb
rileymci <riley.mcilwain@connect.qut.edu.au>
2024-01-16 16:03:04 +1000

    timing change

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,7 +9,7 @@ export const Banner = () => {
     const toRotate = ["Software Engineer", "Electrical Engineer", "Web Developer"];
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300, - Math.random() * 100);
-    const period = 2000;
+    const period = 1500;
 
 
     useEffect(() => {
